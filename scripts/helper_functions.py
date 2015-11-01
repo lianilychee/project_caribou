4a8b916cd8c4df2e3f6671ea0bbf88dc85698077
mafaldaborges <mafaldaborges94@hotmail.com>
2015-10-31 21:58:15 -0400

    resolved merge conflict

--- scripts/helper_functions.py
+++ scripts/helper_functions.py
@@ -3,6 +3,18 @@
 import math
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
+from scipy.linalg import norm
+from scipy import sum, average
+import rospy
+from sensor_msgs.msg import Image
+from copy import deepcopy
+from cv_bridge import CvBridge
+from geometry_msgs.msg import Twist, Vector3
+import match_keypoints as mk
+import rospkg
+
+
 
 def find_line(image, top_left, bottom_right, lower_bound, upper_bound, threshold):
   """
@@ -36,3 +48,37 @@ def find_line(image, top_left, bottom_right, lower_bound, upper_bound, threshold
     return (-(avg_col - centerline), True) # turn left
   else:
     return (0, True) # line could be nonexistent or centered
+
+
+def find_boundary_pts():
+  """
+  Given the bw image, find the boundary points, and output the min/max points 
+  needed to draw a two-point rectangle.
+  """
+
+
+  image = cv2.imread('../stop.png')
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  gray = cv2.GaussianBlur(gray, (3, 3), 0)
+  #cv2.imshow("Gray",gray)
+  #cv2.waitKey(0)
+  edged = cv2.Canny(gray,10,250)
+  #cv2.imshow("Edged", edged)
+  #cv2.waitKey(0)
+  kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+  closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+  #cv2.imshow("Closed",closed)
+  #cv2.waitKey(0)
+  (cnts, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  total = 0
+
+  for c in cnts:
+    peri = cv2.arcLength(c,True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+    if len(approx) == 8:
+      cv2.drawContours(image, [approx], -1, (0, 255, 0), 4)
+      total +=1
+
+  cv2.imshow("Output", image)
+  cv2.waitKey(0)
